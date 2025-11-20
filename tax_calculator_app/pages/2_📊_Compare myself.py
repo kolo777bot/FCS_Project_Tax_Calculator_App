fef5ae55c255270d859a3898dbf0f13fb13e399a
kolo777bot <n.eissing@proton.me>
2025-11-20 09:27:06 +0100

    Create 2_📊_Compare myself.py

---
+++ tax_calculator_app/pages/2_📊_Compare myself.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#Sidebar
+st.set_page_config(page_title="Compare myself", page_icon="📊")
+
+
+#Title
+st.title("📊 Compare myself")
+#Infobox
+st.info("Compare your taxes to persons with similar attributes!")
